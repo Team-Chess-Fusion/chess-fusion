68d769ebc5b2f6a29a68ac951752ac65392e1801
Cort Ferguson <98cort@gmail.com>
2016-07-24 00:58:17 +0000

    Initial commit
    Created new rails app 'chess-fusion', added rails_12factor gem, and
    adjusted production.rb so heroku loads assests.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ChessFusion::Application.config.secret_key_base = '52af5d6f0c9ff229c4b2d40f726a09fcfe1f7ebaff5d1bffe3eec7261c729a9fd7e9ad6e1383b94c7cdf2a2801dee114540682a35ada4048d809dbc34311a36a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ChessFusion::Application.config.session_store :cookie_store, key: '_chess-fusion_session'
