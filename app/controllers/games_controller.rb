3047f4e3499e3966709d9ba476398a39cf48cf14
Adrian Proctor <adrian-proctor@shaw.ca>
2016-07-24 22:02:36 +0000

    Added simpleform, games model

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,23 @@
+class GamesController < ApplicationController
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.create(game_params)
+    if @game.valid?
+      redirect_to_root_path
+    end
+  end
+
+  def show
+    
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:name)
+  end
+
+end
