b6ecbf4e80282147f177d8738ef7385141787bd0
Cort Ferguson <98cort@gmail.com>
2016-07-28 19:35:17 +0000

    Add game_id index to pieces

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160728023902) do
+ActiveRecord::Schema.define(version: 20160728193153) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 20160728023902) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "pieces", ["game_id"], name: "index_pieces_on_game_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
