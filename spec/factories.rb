701f441ff54a989b36a9f0f7fa6d6334d9092519
Joshua Tan <tantohyung@gmail.com>
2016-07-31 00:03:11 +0000

    Added test cases when games are unavailable
    - Include additional test case such that Games.available should be empty if all games are full

--- spec/factories.rb
+++ spec/factories.rb
@@ -11,6 +11,14 @@ FactoryGirl.define do
     sequence :name do |n|
       "AwesomeChessGame#{n}"
     end
+    factory :full_game do
+      white_player_id 1
+      black_player_id 3
+    end
+
+    factory :single_player_game do
+      white_player_id 1
+    end
   end
     
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Game, type: :model do
   let(:user) {FactoryGirl.create(:user)}
+  let(:game) {FactoryGirl.create(:game)}
+  let(:full_game) {FactoryGirl.create(:full_game)}
+  let(:single_player_game) {FactoryGirl.create(:single_player_game)}
   describe "initial test" do
     it "should be true" do
       expect(true).to eq true
@@ -17,9 +20,15 @@ RSpec.describe Game, type: :model do
       expect(Game.available).to include available_game
     end
 
-    it "should return no games if none are available" do
+    it "should return no games if all games are full" do
+      full_game
       expect(Game.available).to be_empty
     end
+
+    it "should return no games if no games have been created" do
+      expect(Game.available).to be_empty
+    end
+    
   end
 
 end
