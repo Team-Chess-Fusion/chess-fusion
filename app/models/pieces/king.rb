ed9bd46d1be3b7ad4ea8cb28d227e2dc812f1bf9
Jon Yu <jy300x2@gmail.com>
2016-08-07 12:11:30 +0000

    Added valid_move? method to King piece

--- app/models/pieces/king.rb
+++ app/models/pieces/king.rb
@@ -1,2 +1,7 @@
 class King < Piece
+  def valid_move?(destination_row, destination_column)
+    current_column_coordinate = column_coordinate
+    current_row_coordinate = row_coordinate
+    (current_row_coordinate - destination_row).abs <= 1 && (current_column_coordinate - destination_column).abs <= 1
+  end
 end
--- spec/models/pieces/king_spec.rb
+++ spec/models/pieces/king_spec.rb
@@ -1,4 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe King, type: :model do
+  before :all do
+    @game = FactoryGirl.create(:game)
+    Piece.destroy_all
+    @king = FactoryGirl.create(:piece, game_id: @game.id, type: 'King', color: 'black', row_coordinate: 3, column_coordinate: 3)
+  end
+
+  describe 'valid_move?' do
+    it 'should return true when the king moves up or down one row' do
+      expect(@king.valid_move?(4, 3)).to eq true
+      expect(@king.valid_move?(2, 3)).to eq true
+    end
+
+    it 'should return true when the king moves left or right one column' do
+      expect(@king.valid_move?(3, 2)).to eq true
+      expect(@king.valid_move?(3, 4)).to eq true
+    end
+
+    it 'should return true when the king moves diagonally' do
+      expect(@king.valid_move?(4, 2)).to eq true
+      expect(@king.valid_move?(4, 4)).to eq true
+      expect(@king.valid_move?(2, 2)).to eq true
+      expect(@king.valid_move?(2, 4)).to eq true
+    end
+
+    it 'should return false when the king moves up or down more than one row' do
+      expect(@king.valid_move?(6, 3)).to eq false
+      expect(@king.valid_move?(1, 3)).to eq false
+    end
+
+    it 'should return false when the king moves left or right more than one column' do
+      expect(@king.valid_move?(3, 1)).to eq false
+      expect(@king.valid_move?(3, 6)).to eq false
+    end
+
+    it 'should return false when the king moves more than one row/column diagonally' do
+      expect(@king.valid_move?(5, 2)).to eq false
+      expect(@king.valid_move?(5, 4)).to eq false
+      expect(@king.valid_move?(1, 2)).to eq false
+      expect(@king.valid_move?(1, 4)).to eq false
+    end
+  end
 end
