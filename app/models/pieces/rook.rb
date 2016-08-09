2dbf28952a66398e55deb53cae850c1bf3ff2d73
Cort Ferguson <98cort@gmail.com>
2016-08-09 01:08:23 +0000

    Make rubocop green

--- app/models/pieces/rook.rb
+++ app/models/pieces/rook.rb
@@ -3,6 +3,6 @@ class Rook < Piece
     row_move = (row_coordinate - row_destination).abs
     col_move = (column_coordinate - column_destination).abs
 
-    (row_move == 0 || col_move == 0)
+    (row_move.zero? || col_move.zero?)
   end
 end
