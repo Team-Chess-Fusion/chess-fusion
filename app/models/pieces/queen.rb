33700c54911daac602a3ff7a403fee76abf8f6d5
Cort Ferguson <98cort@gmail.com>
2016-08-09 01:22:10 +0000

    Initial queen valid_move? method

--- app/models/pieces/queen.rb
+++ app/models/pieces/queen.rb
@@ -1,2 +1,8 @@
 class Queen < Piece
+  def valid_move?(row_destination, column_destination)
+    row_move = (row_coordinate - row_destination).abs
+    col_move = (column_coordinate - column_destination).abs
+
+    (row_move.zero? || col_move.zero?) || (row_move == col_move)
+  end
 end
