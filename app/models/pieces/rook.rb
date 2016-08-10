034e0c454277d5b9f22d5d4d9e1693b6e2ec5d25
Cort Ferguson <98cort@gmail.com>
2016-08-10 17:34:45 +0000

    Rubocop checks

--- app/models/pieces/rook.rb
+++ app/models/pieces/rook.rb
@@ -3,6 +3,6 @@ class Rook < Piece
     row_move = (row_coordinate - row_destination).abs
     col_move = (column_coordinate - column_destination).abs
 
-    (row_move.zero? || col_move.zero?) && (!self.obstructed?(row_destination, column_destination))
+    (row_move.zero? || col_move.zero?) && !obstructed?(row_destination, column_destination)
   end
 end
