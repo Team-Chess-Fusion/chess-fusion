b7a500d08d6e3f6a2a1f8312db3ac3e349a879cf
Adrian Proctor <adrian-proctor@shaw.ca>
2016-08-10 19:23:50 +0000

    Fix devise statement in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-  devise :omniauthable, :omniauth_providers => [:facebook]
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, :omniauth_providers => [:facebook]
 
   has_many :white_player_games, class_name: 'Game', foreign_key: 'white_player_id'
   has_many :black_player_games, class_name: 'Game', foreign_key: 'black_player_id'
