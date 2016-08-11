60e120f7185f49be9d35ad83718487683425429f
Adrian Proctor <adrian-proctor@shaw.ca>
2016-08-11 18:26:56 +0000

    Fix rubocop errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable, :omniauth_providers => [:facebook]
+         :omniauthable, omniauth_providers: [:facebook]
 
   has_many :white_player_games, class_name: 'Game', foreign_key: 'white_player_id'
   has_many :black_player_games, class_name: 'Game', foreign_key: 'black_player_id'
@@ -11,7 +11,7 @@ class User < ActiveRecord::Base
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
+      user.password = Devise.friendly_token[0, 20]
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 ChessFusion::Application.routes.draw do
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
