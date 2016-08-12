ea404387003f35a600e7b2793eb9dfaddb635013
Adrian Proctor <adrian-proctor@shaw.ca>
2016-08-12 02:02:15 +0000

    Replace html_safe with escape parameter

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -7,36 +7,34 @@ module GamesHelper
       if piece.color == 'white'
         case piece.type
         when 'King'
-          content_tag(:div, '&#9812;'.html_safe, class: 'piece-font')
+          content_tag(:div, '&#9812', class: 'piece-font', escape: false)
         when 'Queen'
-          content_tag(:div, '&#9813'.html_safe, class: 'piece-font')
+          content_tag(:div, '&#9813', class: 'piece-font', escape: false)
         when 'Rook'
-          content_tag(:div, '&#9814'.html_safe, class: 'piece-font')
+          content_tag(:div, '&#9814', class: 'piece-font', escape: false)
         when 'Bishop'
-          content_tag(:div, '&#9815'.html_safe, class: 'piece-font')
+          content_tag(:div, '&#9815', class: 'piece-font', escape: false)
         when 'Knight'
-          content_tag(:div, '&#9816'.html_safe, class: 'piece-font')
+          content_tag(:div, '&#9816', class: 'piece-font', escape: false)
         when 'Pawn'
-          content_tag(:div, '&#9817'.html_safe, class: 'piece-font')
+          content_tag(:div, '&#9817', class: 'piece-font', escape: false)
         end
       else
         case piece.type
         when 'King'
-          content_tag(:div, '&#9818'.html_safe, class: 'piece-font')
+          content_tag(:div, '&#9818', class: 'piece-font', escape: false)
         when 'Queen'
-          content_tag(:div, '&#9819'.html_safe, class: 'piece-font')
+          content_tag(:div, '&#9819', class: 'piece-font', escape: false)
         when 'Rook'
-          content_tag(:div, '&#9820'.html_safe, class: 'piece-font')
+          content_tag(:div, '&#9820', class: 'piece-font', escape: false)
         when 'Bishop'
-          content_tag(:div, '&#9821'.html_safe, class: 'piece-font')
+          content_tag(:div, '&#9821', class: 'piece-font', escape: false)
         when 'Knight'
-          content_tag(:div, '&#9822'.html_safe, class: 'piece-font')
+          content_tag(:div, '&#9822', class: 'piece-font', escape: false)
         when 'Pawn'
-          content_tag(:div, '&#9823'.html_safe, class: 'piece-font')
+          content_tag(:div, '&#9823', class: 'piece-font', escape: false)
         end
       end
-
-      # piece.color.capitalize! + ' ' + piece.type
     end
   end
 end
