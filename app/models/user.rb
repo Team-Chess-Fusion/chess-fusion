927da7c9bbfc1663eeedde0e717b4f7757fa4c46
Jon Yu <jy300x2@gmail.com>
2016-08-12 14:48:17 +0000

    Added gravatar gem to display user gravatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,9 @@ class User < ActiveRecord::Base
   has_many :white_player_games, class_name: 'Game', foreign_key: 'white_player_id'
   has_many :black_player_games, class_name: 'Game', foreign_key: 'black_player_id'
 
+  include Gravtastic
+  has_gravatar
+
   def games
     Game.where('white_player_id = ? OR black_player_id = ?', id, id)
   end
