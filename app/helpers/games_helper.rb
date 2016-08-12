e7f755e342ddefbfbe871a1a0a6e0cb3ad7db278
Adrian Proctor <adrian-proctor@shaw.ca>
2016-08-12 19:01:19 +0000

    CSS adjustments for display of pieces

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -4,37 +4,7 @@ module GamesHelper
 
     return '' unless piece.present?
     link_to game_piece_path(game_id: @game, id: @game.piece_at_location(row, col)) do
-      if piece.color == 'white'
-        case piece.type
-        when 'King'
-          content_tag(:div, '&#9812'.html_safe, class: 'piece-font')
-        when 'Queen'
-          content_tag(:div, '&#9813'.html_safe, class: 'piece-font')
-        when 'Rook'
-          content_tag(:div, '&#9814'.html_safe, class: 'piece-font')
-        when 'Bishop'
-          content_tag(:div, '&#9815'.html_safe, class: 'piece-font')
-        when 'Knight'
-          content_tag(:div, '&#9816'.html_safe, class: 'piece-font')
-        when 'Pawn'
-          content_tag(:div, '&#9817'.html_safe, class: 'piece-font')
-        end
-      else
-        case piece.type
-        when 'King'
-          content_tag(:div, '&#9818'.html_safe, class: 'piece-font')
-        when 'Queen'
-          content_tag(:div, '&#9819'.html_safe, class: 'piece-font')
-        when 'Rook'
-          content_tag(:div, '&#9820'.html_safe, class: 'piece-font')
-        when 'Bishop'
-          content_tag(:div, '&#9821'.html_safe, class: 'piece-font')
-        when 'Knight'
-          content_tag(:div, '&#9822'.html_safe, class: 'piece-font')
-        when 'Pawn'
-          content_tag(:div, '&#9823'.html_safe, class: 'piece-font')
-        end
-      end
+      display_piece(piece, 'piece-font')
     end
   end
 end
--- app/helpers/pieces_helper.rb
+++ app/helpers/pieces_helper.rb
@@ -1,4 +1,38 @@
 module PiecesHelper
+  def display_piece(piece, css_class = 'piece-font')
+    if piece.color == 'white'
+      case piece.type
+      when 'King'
+        content_tag(:p, '&#9812'.html_safe, class: css_class)
+      when 'Queen'
+        content_tag(:p, '&#9813'.html_safe, class: css_class)
+      when 'Rook'
+        content_tag(:p, '&#9814'.html_safe, class: css_class)
+      when 'Bishop'
+        content_tag(:p, '&#9815'.html_safe, class: css_class)
+      when 'Knight'
+        content_tag(:p, '&#9816'.html_safe, class: css_class)
+      when 'Pawn'
+        content_tag(:p, '&#9817'.html_safe, class: css_class)
+      end
+    else
+      case piece.type
+      when 'King'
+        content_tag(:p, '&#9818'.html_safe, class: css_class)
+      when 'Queen'
+        content_tag(:p, '&#9819'.html_safe, class: css_class)
+      when 'Rook'
+        content_tag(:p, '&#9820'.html_safe, class: css_class)
+      when 'Bishop'
+        content_tag(:p, '&#9821'.html_safe, class: css_class)
+      when 'Knight'
+        content_tag(:p, '&#9822'.html_safe, class: css_class)
+      when 'Pawn'
+        content_tag(:p, '&#9823'.html_safe, class: css_class)
+      end
+    end
+  end
+
   def render_piece_after_selection(row, col)
     piece = @game.piece_at_location(row, col)
 
@@ -9,11 +43,9 @@ module PiecesHelper
     end unless piece.present?
 
     return link_to game_piece_path(game_id: @game, id: piece) do
-      piece.color.capitalize! + ' ' + piece.type
+      display_piece(piece, 'piece-font')
     end unless piece == @selected_piece
 
-    content_tag :div, class: 'selected-piece' do
-      piece.color.capitalize! + ' ' + piece.type
-    end
+    display_piece(piece, 'selected-piece')
   end
 end
