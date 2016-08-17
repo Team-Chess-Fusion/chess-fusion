db951d535928269395a05c6f9e8ac89c0bc6fa54
Adrian Proctor <adrian-proctor@shaw.ca>
2016-08-17 18:06:32 +0000

    Add database field move_turn for turn based logic

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160810171313) do
+ActiveRecord::Schema.define(version: 20160817180458) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20160810171313) do
     t.datetime "updated_at"
     t.integer  "black_player_id"
     t.integer  "white_player_id"
+    t.string   "move_turn"
   end
 
   add_index "games", ["black_player_id"], name: "index_games_on_black_player_id", using: :btree
