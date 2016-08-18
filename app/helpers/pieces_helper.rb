2e9e1d5f914e79c77b45e26989908b760f294756
Joshua Tan <tantohyung@gmail.com>
2016-08-18 15:15:57 +0000

    Refactor display piece method

--- app/helpers/pieces_helper.rb
+++ app/helpers/pieces_helper.rb
@@ -1,20 +1,7 @@
 module PiecesHelper
   def display_piece(piece, css_class)
     data = piece_path(piece)
-    case piece.type
-    when 'King'
-      css_class += ' ' + piece.color + '-king'
-    when 'Queen'
-      css_class += ' ' + piece.color + '-queen'
-    when 'Rook'
-      css_class += ' ' + piece.color + '-rook'
-    when 'Bishop'
-      css_class += ' ' + piece.color + '-bishop'
-    when 'Knight'
-      css_class += ' ' + piece.color + '-knight'
-    when 'Pawn'
-      css_class += ' ' + piece.color + '-pawn'
-    end
+    css_class += ' ' + piece.color + '-' + piece.type.downcase!
     content_tag(:p, '', class: css_class, data: { update_url: data })
   end
 
