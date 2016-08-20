f958bc10e2eb975bbb0f49dd2e77d1f0be3016c9
Joshua Tan <tantohyung@gmail.com>
2016-08-20 23:21:05 +0000

    Clean up code to improve readability

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -2,9 +2,9 @@ class PiecesController < ApplicationController
   before_action :authenticate_user!
   def update
     @piece = Piece.find(params[:id])
-    return render json: { update_attempt: 'invalid move' } if @piece.valid_move?(params[:piece][:row_coordinate].to_i, params[:piece][:column_coordinate].to_i) == false
+    return render json: { update_attempt: 'invalid move' } unless @piece.valid_move?(params[:piece][:row_coordinate].to_i, params[:piece][:column_coordinate].to_i)
     @piece.update_attributes(piece_params)
-    in_check = @piece.game.in_check?.present? ? true : false
+    in_check = @piece.game.in_check?.present?
 
     render json: { update_attempt: 'success', in_check: in_check }
   end
