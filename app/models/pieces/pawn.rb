fd68bad84327ea6470e7dc86991e4f532287238b
Jon Yu <jy300x2@gmail.com>
2016-09-07 01:46:51 +0000

    Continued to refactor en passant methods in pawn.rb

--- app/models/pieces/pawn.rb
+++ app/models/pieces/pawn.rb
@@ -11,58 +11,55 @@ class Pawn < Piece
     end
   end
 
-  def check_left_for_pawn(column_coordinate)
+  def check_left_right_pawn(column_coordinate)
     opposing_color = game.opposite_color(color)
     left = game.pieces.find_by(column_coordinate: column_coordinate - 1, row_coordinate: row_coordinate)
-    left.type == 'Pawn' && left.color == opposing_color
-  end
-
-  def check_right_for_pawn(column_coordinate)
-    opposing_color = game.opposite_color(color)
     right = game.pieces.find_by(column_coordinate: column_coordinate + 1, row_coordinate: row_coordinate)
-    right.type == 'Pawn' && right.color == opposing_color
-  end
-
-  def white_pawn_capture_enpassant
-    return false if row_coordinate != 4
-    if square_taken?(row_coordinate, column_coordinate - 1)
-      check_left_for_pawn(column_coordinate)
-      opposing_pawn = check_adjacent_left(column_coordinate)
-      return false if opposing_pawn.en_passant == false
-    elsif square_taken?(row_coordinate, column_coordinate + 1)
-      check_right_for_pawn(column_coordinate)
-      opposing_pawn = check_adjacent_right(column_coordinate)
-      return false if opposing_pawn.en_passant == false
+    if !left.nil?
+      left.type == 'Pawn' && left.color == opposing_color
+    elsif !right.nil?
+      right.type == 'Pawn' && right.color == opposing_color
     else
       return false
     end
-    move_to!(opposing_pawn.row_coordinate + 1, opposing_pawn.column_coordinate)
+  end
+
+  def enpassant_left
+    check_left_right_pawn(column_coordinate)
+    opposing_pawn = game.pieces.find_by(column_coordinate: column_coordinate - 1, row_coordinate: row_coordinate)
+    return false if opposing_pawn.en_passant == false
+    add_coordinate = color == 'black' ? -1 : 1
+    move_to!((opposing_pawn.row_coordinate + add_coordinate), opposing_pawn.column_coordinate)
     opposing_pawn.update_attributes(row_coordinate: nil, column_coordinate: nil)
   end
 
-  def black_pawn_capture_enpassant
-    return false if row_coordinate != 3
+  def enpassant_right
+    check_left_right_pawn(column_coordinate)
+    opposing_pawn = game.pieces.find_by(column_coordinate: column_coordinate + 1, row_coordinate: row_coordinate)
+    return false if opposing_pawn.en_passant == false
+    add_coordinate = color == 'black' ? -1 : 1
+    move_to!((opposing_pawn.row_coordinate + add_coordinate), opposing_pawn.column_coordinate)
+    opposing_pawn.update_attributes(row_coordinate: nil, column_coordinate: nil)
+  end
+
+  def capture_enpassant
     if square_taken?(row_coordinate, column_coordinate - 1)
-      check_left_for_pawn(column_coordinate)
-      opposing_pawn = check_adjacent_left(column_coordinate)
-      return false if opposing_pawn.en_passant == false
+      enpassant_left
     elsif square_taken?(row_coordinate, column_coordinate + 1)
-      check_right_for_pawn(column_coordinate)
-      opposing_pawn = check_adjacent_right(column_coordinate)
-      return false if opposing_pawn.en_passant == false
+      enpassant_right
     else
       return false
     end
-    move_to!(opposing_pawn.row_coordinate - 1, opposing_pawn.column_coordinate)
-    opposing_pawn.update_attributes(row_coordinate: nil, column_coordinate: nil)
   end
 
   def capture_for_enpassant
     case color
     when 'white'
-      white_pawn_capture_enpassant
+      return false if row_coordinate != 4
+      capture_enpassant
     when 'black'
-      black_pawn_capture_enpassant
+      return false if row_coordinate != 3
+      capture_enpassant
     end
   end
 
