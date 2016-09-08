e50d82bb3fbbe75c030aafe3ba0c39b69a864dec
Adrian Proctor <adrian-proctor@shaw.ca>
2016-09-08 23:11:21 +0000

    Initial version - refactor piece.rb model

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -68,22 +68,14 @@ class Piece < ActiveRecord::Base
   def check_diagonal(destination_row, destination_column)
     slope = (destination_column - column_coordinate) / (destination_row - row_coordinate)
     start_x = [row_coordinate, destination_row].min + 1
-    if slope > 0
-      end_x = [row_coordinate, destination_row].max - 1
-      start_y = [column_coordinate, destination_column].min + 1
-      while start_x <= end_x
-        return true if square_taken?(start_x, start_y)
-        start_x += 1
-        start_y += 1
-      end
-    else
-      end_x = [row_coordinate, destination_row].max - 1
-      start_y = [column_coordinate, destination_column].max - 1
-      while start_x <= end_x
-        return true if square_taken?(start_x, start_y)
-        start_x += 1
-        start_y -= 1
-      end
+    start_y_increment = slope > 0 ? 1 : -1
+    start_y = slope > 0 ? [column_coordinate, destination_column].min + start_y_increment : [column_coordinate, destination_column].max + start_y_increment
+    end_x = [row_coordinate, destination_row].max - 1
+
+    while start_x <= end_x
+      return true if square_taken?(start_x, start_y)
+      start_x += 1
+      start_y += start_y_increment
     end
     false
   end
